25a65fddace14e28f466a47f618398fa5b6a381c
Jorciney <jorciney.dias@hotmail.com>
2019-03-01 13:33:22 +0100

    Updates

---
+++ precache-manifest.9c0811cfe52c3d8b1a7cedf862be9f0b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6b2d5e24dcd5fe7af099",
+    "url": "/React-basics/static/js/runtime~main.6b2d5e24.js"
+  },
+  {
+    "revision": "93281a52bf5d7cb42a8e",
+    "url": "/React-basics/static/js/main.93281a52.chunk.js"
+  },
+  {
+    "revision": "c18586d07dfd1c2a27a8",
+    "url": "/React-basics/static/js/2.c18586d0.chunk.js"
+  },
+  {
+    "revision": "93281a52bf5d7cb42a8e",
+    "url": "/React-basics/static/css/main.6ba718a2.chunk.css"
+  },
+  {
+    "revision": "a35bbda6f7727c48249f74f9905768f4",
+    "url": "/React-basics/index.html"
+  }
+];
