a3046cb4794d09a349a1a23b376d4ea4cffbea77
Jorciney <jorciney.dias@hotmail.com>
2019-03-01 13:23:57 +0100

    Filtered the  list of news

--- src/index.js
+++ src/index.js
@@ -10,19 +10,25 @@ import NewsList from './components/news_list';
 class App extends Component {
     state = {
         news: JSON,
+        filteredNews: [],
         searchTerm: undefined
     };
 
     getKeyWord = (event) => {
-        this.setState({ searchTerm: event.target.value });
-        return this.searchTerm;
+        const searchTerm = event.target.value;
+        let filteredNews = this.state.news.filter(item => {
+            return item.title.indexOf(searchTerm) > -1;
+        });
+        this.setState({filteredNews: filteredNews});
+        return searchTerm;
     };
 
     render() {
+        let newsList = this.state.filteredNews.length <= 0 ? this.state.news : this.state.filteredNews;
         return (
             <div className="lowText">
                 <Header searchTerm={this.state.searchTerm} keywords={this.getKeyWord}/>
-                <NewsList news={this.state.news}/>
+                <NewsList news={newsList}/>
                 <div>{this.state.searchTerm}</div>
             </div>
         )
